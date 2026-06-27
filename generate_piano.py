9c81a5fe4cc6d2bd6e63728e444dded2fe27319a
aspend <aspenrdevries@gmail.com>
2026-06-26 22:50:34 -0700

    Update musical logic in generate_piano.py

--- generate_piano.py
+++ generate_piano.py
@@ -1,3 +1,4 @@
+import random
 import subprocess
 import sys
 from music21 import stream, note, chord, meter, key, tempo, instrument
@@ -15,10 +16,32 @@ right.insert(0, instrument.Piano())
 left = stream.Part()
 left.insert(0, instrument.Piano())
 
-melody = ["C5", "D5", "E5", "G5", "E5", "D5", "C5", "G4"]
+scale = ["C5", "D5", "E5", "F5", "G5", "A5", "B5", "C6"]
 
-for pitch in melody:
-    right.append(note.Note(pitch, quarterLength=1))
+chord_tones_by_bar = [
+    ["C5", "E5", "G5"],
+    ["G5", "B5", "D5"],
+    ["A5", "C5", "E5"],
+    ["F5", "A5", "C5"],
+]
+
+def make_melody_bar(chord_tones):
+    bar = []
+
+    for beat in range(4):
+        # Strong beats should usually be chord tones
+        if beat == 0 or beat == 2:
+            pitch = random.choice(chord_tones)
+        else:
+            pitch = random.choice(scale)
+
+        bar.append(note.Note(pitch, quarterLength=1))
+
+    return bar
+
+for chord_tones in chord_tones_by_bar * 4:
+    for n in make_melody_bar(chord_tones):
+        right.append(n)
 
 chords = [
     ["C3", "E3", "G3"],
@@ -27,8 +50,15 @@ chords = [
     ["F2", "A2", "C3"],
 ]
 
-for c in chords * 2:
-    left.append(chord.Chord(c, quarterLength=2))
+for c in chords * 4:
+    root = c[0]
+    third = c[1]
+    fifth = c[2]
+
+    left.append(note.Note(root, quarterLength=1))
+    left.append(note.Note(fifth, quarterLength=1))
+    left.append(note.Note(third, quarterLength=1))
+    left.append(note.Note(fifth, quarterLength=1))
 
 score.insert(0, right)
 score.insert(0, left)
@@ -45,5 +75,3 @@ if open_score:
         "MuseScore 4",
         "piano_piece.musicxml"
     ])
-
-
